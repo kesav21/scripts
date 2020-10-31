42b93585d12362d4a42797375c1bc5077a067e90
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-10-31 03:54:00 -0700

    chore: handle errors with Either

--- wm/ChangeBrightness.hs
+++ wm/ChangeBrightness.hs
@@ -1,11 +1,29 @@
 #!/usr/bin/env runghc
 
 import           System.Environment             ( getArgs )
+import           System.IO                      ( openFile
+                                                , IOMode(ReadMode, WriteMode)
+                                                , hGetContents
+                                                , hPutStr
+                                                , hClose
+                                                )
 import           System.Exit                    ( exitFailure )
 import           Text.Read                      ( readMaybe )
 
 -- https://www.schoolofhaskell.com/user/bartosz/basics-of-haskell/3-pure-functions-laziness-io
 -- https://www.schoolofhaskell.com/school/starting-with-haskell/basics-of-haskell/10_Error_Handling
+
+toEither :: String -> Maybe a -> Either String a
+toEither msg Nothing      = Left msg
+toEither msg (Just first) = Right first
+
+argError = toEither "Provide an argument of the form \\+\\d+%"
+signError = toEither "Provide an argument of the form \\+\\d+%"
+magError = toEither "Provide an argument of the form \\+\\d+%"
+percentError = toEither "Provide an argument of the form \\+\\d+%"
+readCurrentError = toEither "Failed to read current brightness"
+readFullError = toEither "Failed to read max brightness "
+staleError = toEither "No change in brightness"
 
 toInt :: String -> Maybe Int
 toInt = readMaybe
@@ -37,12 +55,12 @@ isPercent :: Char -> Maybe Int
 isPercent '%' = Just 1
 isPercent _   = Nothing
 
-parseArgs :: [String] -> Maybe Int
+parseArgs :: [String] -> Either String Int
 parseArgs args = do
-  first     <- head' args
-  sign'     <- head' first >>= sign
-  magnitude <- midl' first >>= toInt
-  percent   <- last' first >>= isPercent
+  first     <- argError (head' args)
+  sign'     <- signError (head' first >>= sign)
+  magnitude <- magError (midl' first >>= toInt)
+  percent   <- percentError (last' first >>= isPercent)
   return $ sign' * magnitude * percent
 
 restrict :: Int -> Int -> Int -> Int
@@ -54,22 +72,33 @@ stale :: Int -> Int -> Maybe Int
 stale current new | current == new = Nothing
                   | otherwise      = Just new
 
-calculate :: [String] -> String -> String -> Maybe Int
+calculate :: [String] -> String -> String -> Either String Int
 calculate args current full = do
   args'    <- parseArgs args
-  current' <- toInt current
-  full'    <- toInt full
-  stale current' . restrict 0 full' $ current' + args' * full' `div` 100
+  current' <- readCurrentError (toInt current)
+  full'    <- readFullError (toInt full)
+  staleError . stale current' . restrict 0 full' $ current' + args' * full' `div` 100
 
 main = do
   args    <- getArgs
-  current <- readFile "/sys/class/backlight/intel_backlight/brightness"
-  full    <- readFile "/sys/class/backlight/intel_backlight/max_brightness"
+
+  currH   <- openFile "/sys/class/backlight/intel_backlight/brightness" ReadMode
+  current <- hGetContents currH
+
+  fullH   <- openFile "/sys/class/backlight/intel_backlight/max_brightness" ReadMode
+  full    <- hGetContents fullH
 
   case calculate args current full of
-    Nothing -> do
-      putStrLn "error"
+    Left e -> do
+      hClose currH
+      hClose fullH
+      putStrLn e
       exitFailure
-    Just x -> do
+    Right x -> do
+      hClose currH
+      hClose fullH
       print x
-      writeFile "/sys/class/backlight/intel_backlight/brightness" (show x)
+      currH <- openFile "/sys/class/backlight/intel_backlight/brightness" WriteMode
+      hPutStr currH (show x)
+      hClose currH
+
