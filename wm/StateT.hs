cab2dc2c16016e5ab8ad7089d54e0d68259621b5
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-11-27 00:26:34 -0700

    chore: finish StateT example with MaybeT

--- wm/StateT.hs
+++ wm/StateT.hs
@@ -1,16 +1,16 @@
 #!/usr/bin/env runghc
 
+import           Control.Monad.Trans.Maybe
+import           Text.Read                      ( readMaybe )
 import           System.Environment             ( getArgs )
-import           Control.Applicative            ( empty
-                                                , (<|>)
-                                                )
+import           System.Exit                    ( exitFailure )
+import           Control.Applicative            ( empty )
 import           Control.Monad.State            ( StateT
                                                 , get
                                                 , put
                                                 , runStateT
                                                 )
 import           Data.Char                      ( isDigit )
-import           Data.Maybe                     ( catMaybes )
 
 type Parser = StateT String Maybe
 
@@ -58,10 +58,45 @@ parseInput = do
   magnitude <- parseMagnitude
   return $ sign * magnitude
 
-parseArgs :: [String] -> Maybe Int
-parseArgs = fmap fst . runStateT parseInput . unwords
+parseArgs :: IO [String] -> MaybeT IO Int
+parseArgs = MaybeT . fmap (fmap fst . runStateT parseInput . unwords)
 
-main = getArgs >>= print . parseArgs
+readFileInt :: String -> MaybeT IO Int
+readFileInt = MaybeT . fmap readMaybe . readFile
+
+type BrightCalc = StateT (Int, Int, Int) (MaybeT IO)
+
+applyDiff :: BrightCalc Int
+applyDiff = do
+  (curr, full, diff) <- get
+  return $ curr + diff * full `div` 100
+
+restrict :: Int -> BrightCalc Int
+restrict val = do
+  (_, full, _) <- get
+  if val < 0 then return 0 else if val > full then return full else return val
+
+stale :: Int -> BrightCalc Int
+stale val = do
+  (curr, _, _) <- get
+  if val == curr then empty else return val
+
+newBrightness :: (Int, Int, Int) -> MaybeT IO Int
+newBrightness = fmap fst . runStateT (applyDiff >>= restrict >>= stale)
+
+main = do
+  let currPath = "/sys/class/backlight/intel_backlight/brightness"
+      fullPath = "/sys/class/backlight/intel_backlight/max_brightness"
+
+  state <- runMaybeT $ do
+    curr <- readFileInt currPath
+    full <- readFileInt fullPath
+    diff <- parseArgs getArgs
+    newBrightness (curr, full, diff)
+
+  case state of
+    Nothing -> exitFailure
+    Just a  -> writeFile currPath (show a)
 
 -- main = do
 --   print $ runStateT (parseChar '+') ""
