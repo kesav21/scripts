7cd77942c76f9a082579182494daef9f27e9b4a0
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-13 17:54:34 -0700

    chore(nvim): remove unused code

--- audio/src/callbacks.c
+++ audio/src/callbacks.c
@@ -1,18 +1,22 @@
 #include "callbacks.h"
 
+int intlen(int input) {
+	if (input == 0 || input == 1) {
+		return 1;
+	} else {
+		return ceil(log(input) / log(10));
+	}
+}
+
 void exit_signal_callback(pa_mainloop_api *m, pa_signal_event *e, int sig, void *userdata) {
 	m->quit(m, 0);
 }
 
 void write_sink_callback(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
 	if (i) {
-		int digits;
-		if (i->index == 0 || i->index == 1) digits = 1;
-		else digits = ceil(log(i->index) / log(10));
-
 		int pid;
 		int ret;
-
+		int digits = intlen(i->index);
 		int volume = round(100.0f * (float) pa_cvolume_avg(&(i->volume)) / (float) PA_VOLUME_NORM);;
 		int logpath_description_length;
 		int logpath_volume_length;
@@ -77,9 +81,6 @@ void write_sink_callback(pa_context *c, const pa_sink_info *i, int eol, void *us
 	}
 }
 
-// void destroy_sink_callback(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
-// }
-
 void write_newest_sink_callback(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
 	if (i) {
 		int logpath_newest_sink_length;
@@ -105,14 +106,6 @@ void write_newest_sink_callback(pa_context *c, const pa_sink_info *i, int eol, v
 }
 
 #define NOTIFICATION_ID 1005
-
-int intlen(int input) {
-	if (input == 0 || input == 1) {
-		return 1;
-	} else {
-		return ceil(log(input) / log(10));
-	}
-}
 
 void notify_sink_callback(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
 	if (i) {
@@ -148,57 +141,6 @@ void switch_sink_inputs_callback(pa_context *c, const pa_sink_input_info *i, int
 	}
 }
 
-// void write_sink_callback(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
-// 	if (i) {
-// 		int pid, ret;
-// 		FILE* file;
-// 		PulseAudio* pa = (PulseAudio*) userdata;
-
-// 		printf("[write_sink]\t%d \"%s\" %.0f %d\n", i->index, i->description, volume, i->mute);
-
-// 		file = fopen(pa->logpath_index, "w");
-// 		if (file) {
-// 			fprintf(file, "%d\n", i->index);
-// 			fclose(file);
-// 		} else {
-// 			fprintf(stderr, "[write_sink]\tfailed writing to %s\n", pa->logpath_index);
-// 		}
-
-// 		file = fopen(pa->logpath_description, "w");
-// 		if (file) {
-// 			fprintf(file, "%s\n", i->description);
-// 			fclose(file);
-// 		} else {
-// 			fprintf(stderr, "[write_sink]\tfailed writing to %s\n", pa->logpath_description);
-// 		}
-
-// 		file = fopen(pa->logpath_volume, "w");
-// 		if (file) {
-// 			fprintf(file, "%.0f\n", volume);
-// 			fclose(file);
-// 		} else {
-// 			fprintf(stderr, "[write_sink]\tfailed writing to %s\n", pa->logpath_volume);
-// 		}
-
-// 		file = fopen(pa->logpath_mute, "w");
-// 		if (file) {
-// 			fprintf(file, "%d\n", i->mute);
-// 			fclose(file);
-// 		} else {
-// 			fprintf(stderr, "[write_sink]\tfailed writing to %s\n", pa->logpath_mute);
-// 		}
-
-// 		pid = pidof("dwmblocks", 9);
-// 		if (pid != -1) {
-// 			printf("[write_sink]\tsignalling dwmblocks (%d)\n", pid);
-// 			ret = kill(pid, SIGRTMIN+1);
-// 			if (ret != 0) {
-// 				fprintf(stderr, "[write_sink]\tfailed to signal dwmblocks (%d)\n", ret);
-// 			}
-// 		}
-// 	}
-// }
-
 void select_sink_callback(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
 	PulseAudio* pa = (PulseAudio*) userdata;
 	if (i) {
@@ -230,7 +172,6 @@ void subscribe_callback(pa_context *c, pa_subscription_event_type_t type, uint32
 	if (eventtype == PA_SUBSCRIPTION_EVENT_REMOVE) {
 		printf("[subscribe]\tremoved sink %d\n", idx);
 		pa_context_get_sink_info_list(c, select_sink_callback, userdata);
-		// pa_context_get_sink_info_list(c, destroy_sink_callback, userdata);
 	}
 
 	if (eventtype == PA_SUBSCRIPTION_EVENT_CHANGE) {
@@ -240,13 +181,11 @@ void subscribe_callback(pa_context *c, pa_subscription_event_type_t type, uint32
 }
 
 void context_state_callback(pa_context *c, void *userdata) {
-	PulseAudio* pa = (PulseAudio*) userdata;
 	if (pa_context_get_state(c) == PA_CONTEXT_READY) {
 		printf("[context_state]\tready\n");
 		pa_context_get_sink_info_list(c, select_sink_callback, userdata);
 		pa_context_set_subscribe_callback(c, subscribe_callback, userdata);
 		pa_context_subscribe(c, PA_SUBSCRIPTION_MASK_SINK, NULL, NULL);
-
 	}
 }
 
