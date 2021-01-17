5057ba4e35db61c5442598ab55ff725da9abb84c
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-16 17:48:09 -0700

    chore: move variable declarations together

--- audio/src/main.c
+++ audio/src/main.c
@@ -1,9 +1,12 @@
 #include "core.h"
 
 int main(int argc, char *argv[]) {
-	PulseAudio* pa = (PulseAudio*) malloc(sizeof(PulseAudio));
+	int ret;
+	PulseAudio* pa;
+
+	pa = (PulseAudio*) malloc(sizeof(PulseAudio));
 	initialize(pa);
-	int ret = run(pa);
+	ret = run(pa);
 	destroy(pa);
 	free(pa);
 	pa = NULL;
