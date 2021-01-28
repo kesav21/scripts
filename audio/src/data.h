16feb2a6a8f402285940589651545302551d968e
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-27 17:48:28 -0700

    chore(audio): remove pa variables from userdata

--- audio/src/data.h
+++ audio/src/data.h
@@ -1,14 +1,7 @@
 #ifndef DATA_H
 #define DATA_H
 
-#include <pulse/pulseaudio.h>
-
 typedef struct {
-	pa_mainloop* _mainloop;
-	pa_mainloop_api* _mainloop_api;
-	pa_context* _context;
-	pa_signal_event* _signal;
-
 	int sink;
 } PulseAudio;
 
--- audio/src/main.c
+++ audio/src/main.c
@@ -3,27 +3,31 @@
 
 int main(int argc, char *argv[]) {
 	int ret;
+	pa_mainloop* mainloop;
+	pa_mainloop_api* mainloop_api;
+	pa_context* context;
+	pa_signal_event* my_signal;
 	PulseAudio* pa;
 
 	pa = (PulseAudio*) malloc(sizeof(PulseAudio));
 
-	pa->_mainloop = pa_mainloop_new();
-	pa->_mainloop_api = pa_mainloop_get_api(pa->_mainloop);
+	mainloop = pa_mainloop_new();
+	mainloop_api = pa_mainloop_get_api(mainloop);
 
-	pa_signal_init(pa->_mainloop_api);
-	pa->_signal = pa_signal_new(SIGINT, exit_signal_callback, pa);
+	pa_signal_init(mainloop_api);
+	my_signal = pa_signal_new(SIGINT, exit_signal_callback, pa);
 	signal(SIGPIPE, SIG_IGN);
 
-	pa->_context = pa_context_new(pa->_mainloop_api, "PulseAudio Test");
-	pa_context_connect(pa->_context, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL);
-	pa_context_set_state_callback(pa->_context, context_state_callback, pa);
+	context = pa_context_new(mainloop_api, "PulseAudio Test");
+	pa_context_connect(context, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL);
+	pa_context_set_state_callback(context, context_state_callback, pa);
 
-	pa_mainloop_run(pa->_mainloop, &ret);
+	pa_mainloop_run(mainloop, &ret);
 
-	pa_context_unref(pa->_context);
-	pa_signal_free(pa->_signal);
+	pa_context_unref(context);
+	pa_signal_free(my_signal);
 	pa_signal_done();
-	pa_mainloop_free(pa->_mainloop);
+	pa_mainloop_free(mainloop);
 	free(pa);
 
 	return ret;
