1d0d45c48e260849e70ac2627d6bfabf45b27307
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-27 17:36:02 -0700

    chore(audio): merge core.{c,h} into main.c

--- audio/src/main.c
+++ audio/src/main.c
@@ -1,17 +1,30 @@
+#include "callbacks.h"
 #include "data.h"
-#include "core.h"
 
 int main(int argc, char *argv[]) {
 	int ret;
 	PulseAudio* pa;
 
 	pa = (PulseAudio*) malloc(sizeof(PulseAudio));
-	initialize(pa);
-	ret = run(pa);
 
-	destroy(pa);
+	pa->_mainloop = pa_mainloop_new();
+	pa->_mainloop_api = pa_mainloop_get_api(pa->_mainloop);
+
+	pa_signal_init(pa->_mainloop_api);
+	pa->_signal = pa_signal_new(SIGINT, exit_signal_callback, pa);
+	signal(SIGPIPE, SIG_IGN);
+
+	pa->_context = pa_context_new(pa->_mainloop_api, "PulseAudio Test");
+	pa_context_connect(pa->_context, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL);
+	pa_context_set_state_callback(pa->_context, context_state_callback, pa);
+
+	pa_mainloop_run(pa->_mainloop, &ret);
+
+	pa_context_unref(pa->_context);
+	pa_signal_free(pa->_signal);
+	pa_signal_done();
+	pa_mainloop_free(pa->_mainloop);
 	free(pa);
-	pa = NULL;
 
 	return ret;
 }
